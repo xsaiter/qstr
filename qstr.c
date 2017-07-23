365c9e91f2b1c8cc2d7f75a5a5baacfd6db07e1c
xsaiter <xsaiter@mail.ru>
2017-07-23 11:53:13 +0300

    format

--- qstr.c
+++ qstr.c
@@ -21,7 +21,7 @@ static int levenshtein_distance(const char *from, const char *to)
     n = strlen(from) + 1;
     m = strlen(to) + 1;
 
-    int d[n][m];   
+    int d[n][m];
 
     for (i = 0; i < n; ++i) {
         for (j = 0; j < m; ++j) {
@@ -35,31 +35,32 @@ static int levenshtein_distance(const char *from, const char *to)
                 int p = d[i][j - 1] + 1;
                 int q = d[i - 1][j] + 1;
                 int r = d[i - 1][j - 1] + ((from[i - 1] == to[j - 1]) ? 0 : 1);
-                int smallest = p;                
+                int smallest = p;
                 if (q < smallest) {
-                    smallest = q;                   
+                    smallest = q;
                 }
 
                 if (r < smallest) {
-                    smallest = r;                  
+                    smallest = r;
                 }
 
-                d[i][j] = smallest;                
+                d[i][j] = smallest;
             }
         }
     }
 
-    return d[n - 1][m - 1];    
+    return d[n - 1][m - 1];
 }
 
-static int least_to_palindrome(const char *s, int i, int j){
-    if(i >= j - 1){
+static int least_to_palindrome(const char *s, int i, int j)
+{
+    if (i >= j - 1) {
         return 0;
-    }           
-    if(s[i] == s[j-1]){
-        return least_to_palindrome(s, i + 1, j-1);
-    }    
-    return Min(least_to_palindrome(s, i+1, j), least_to_palindrome(s, i, j-1)) + 1;
+    }
+    if (s[i] == s[j - 1]) {
+        return least_to_palindrome(s, i + 1, j - 1);
+    }
+    return Min(least_to_palindrome(s, i + 1, j), least_to_palindrome(s, i, j - 1)) + 1;
 }
 
 static int str_is_palindrome(const char *s)
@@ -77,26 +78,27 @@ PG_FUNCTION_INFO_V1(q_str_levenshtein_distance);
 PG_FUNCTION_INFO_V1(q_str_least_to_palindrome);
 PG_FUNCTION_INFO_V1(q_str_is_palindrome);
 
-Datum 
+Datum
 q_str_levenshtein_distance(PG_FUNCTION_ARGS)
-{            
-    char *from = text_to_cstring(PG_GETARG_TEXT_PP(0));        
+{
+    char *from = text_to_cstring(PG_GETARG_TEXT_PP(0));
     char *to = text_to_cstring(PG_GETARG_TEXT_PP(1));
     int distance = levenshtein_distance(from, to);
     PG_RETURN_INT32(distance);
 }
 
-Datum 
+Datum
 q_str_least_to_palindrome(PG_FUNCTION_ARGS)
-{            
+{
     char *s = text_to_cstring(PG_GETARG_TEXT_PP(0));
     int len = strlen(s);
     int least = least_to_palindrome(s, 0, len);
     PG_RETURN_INT32(least);
 }
 
-Datum 
-q_str_is_palindrome(PG_FUNCTION_ARGS){
+Datum
+q_str_is_palindrome(PG_FUNCTION_ARGS)
+{
     char *s = text_to_cstring(PG_GETARG_TEXT_PP(0));
     int res = str_is_palindrome(s);
     PG_RETURN_BOOL(res);
